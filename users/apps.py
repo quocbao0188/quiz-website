3039fd9b816c0d39695b2d872744bc8f79169a7b
Huynh Quoc Bao <huynhquocbao0188@gmail.com>
2020-03-10 14:39:19 +0700

    Add avatar for profile

--- users/apps.py
+++ users/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    #import hàm tạo profile khi tạo tài khoản vào app.py
+    def ready(self):
+        import users.signals
--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+# Hàm tạo profile cho tài khoản
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    objects = models.Manager()
 
     def __str__(self):
         return f'{self.user.username} Profile'
